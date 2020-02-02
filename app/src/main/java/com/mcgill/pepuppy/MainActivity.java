68af5051329bb0aa6bf4aef9b909a1b941d393e3
Eloisaaa <zichen.he@mail.mcgill.ca>
2020-02-01 22:09:55 -0500

    Enhance the user interface.

--- app/src/main/java/com/mcgill/pepuppy/MainActivity.java
+++ app/src/main/java/com/mcgill/pepuppy/MainActivity.java
@@ -9,7 +9,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -39,7 +38,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_main);
+        setContentView(R.layout.activity_main);
 
     }
 
